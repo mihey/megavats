704380887f2eecdd41aafce2bf6e6b7e7c85d881
mihey <kolebaeff@mail>
2023-02-14 17:59:58 +0300

    fix history params

--- lib/megavats/history.rb
+++ lib/megavats/history.rb
@@ -1,13 +1,24 @@
 module Megavats
   module History
+    AVAILABLE_PARAMS = [:start,:end,:period,:type,:limit]
+
     extend self
 
+
     def outer(params={})
-      Megavats::Request.new("/history/json",'Get', true, params).json
+      Megavats::Request.new("/history/json#{params_string(params)}",'Get', true, {}).json
     end
 
     def inner(params={})
-      Megavats::Request.new("/history/inner/json",'Get', true, params).json
+      Megavats::Request.new("/history/inner/json#{params_string(params)}",'Get', true, {}).json
+    end
+
+    def params_string(params)
+      arr = []
+      params.keys.each do |key|
+        arr << "#{key}=#{params[key]}" if Megavats::History::AVAILABLE_PARAMS.include?(key)
+      end
+      arr.size > 0 ? "?#{arr.join('&')}" : ''
     end
 
   end
